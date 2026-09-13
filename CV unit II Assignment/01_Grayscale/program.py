cadc7cb57d419512e063be2e26cb372b9cae3a6c
Adarsh <jadeyuri150@gmail.com>
2026-09-13 14:10:58 +0530

    Update program.py

--- CV unit II Assignment/01_Grayscale/program.py
+++ CV unit II Assignment/01_Grayscale/program.py
@@ -1,9 +1,8 @@
 import cv2
 
-img = cv2.imread("input.jpeg")
-gray = cv2.imread("input.jpeg", 0)
+img = cv2.imread("input.jpg")
+gray = cv2.imread("input.jpg", 0)
 print("original image:", img.shape)
 print("grayscale image:", gray.shape)
 cv2.imwrite("output.png", gray)
-cv2.waitKey()
-cv2.destroyAllWindows()
+
