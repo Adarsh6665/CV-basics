97dc6411b2919d59997e247a3e56669b74d8c02d
Adarsh <jadeyuri150@gmail.com>
2026-09-13 14:12:17 +0530

    Update program.py

--- CV unit II Assignment/03_Contrast_Stretching/program.py
+++ CV unit II Assignment/03_Contrast_Stretching/program.py
@@ -1,13 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("input.jpeg")
-greyscale = cv2.imread("input.jpeg", 0)
+img = cv2.imread("input.jpg")
+greyscale = cv2.imread("input.jpg", 0)
 min = np.min(greyscale)
 max = np.max(greyscale)
 strech = (greyscale - min) * (255.0 / (max - min))
 strech = strech.astype(np.uint8)
 cv2.imwrite("input.jpeg", greyscale)
 cv2.imwrite("output.png", strech)
-cv2.waitKey()
-cv2.destroyAllWindows()
+
