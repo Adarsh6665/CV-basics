7d34db9b4987c8d32ade6ccb49bac921d24f2692
Adarsh <jadeyuri150@gmail.com>
2026-09-14 12:11:27 +0530

    Update program.py

--- CV unit II Assignment/03_Contrast_Stretching/program.py
+++ CV unit II Assignment/03_Contrast_Stretching/program.py
@@ -7,6 +7,6 @@ min = np.min(greyscale)
 max = np.max(greyscale)
 strech = (greyscale - min) * (255.0 / (max - min))
 strech = strech.astype(np.uint8)
-cv2.imwrite("input.jpeg", greyscale)
+cv2.imwrite("input.jpg", greyscale)
 cv2.imwrite("output.png", strech)
 
